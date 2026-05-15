3e2c43727494b734919d9bf8fa8c40d9cd5340f4
Oliver Clark <81559120+OliDClark@users.noreply.github.com>
2026-05-15 17:27:14 +0100

    Bump app version from v1.0.7 to v1.0.8

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Parse.initialize(PARSE_CONFIG.appId, PARSE_CONFIG.javascriptKey);
 Parse.serverURL = PARSE_CONFIG.serverURL;
 console.log('Parse Config:', PARSE_CONFIG);
 
-const APP_VERSION = 'v1.0.7';
+const APP_VERSION = 'v1.0.8';
 const SCOREBOARD_QUERY_LIMIT = 10000;
 const EUROVISION_POINTS_MULTIPLIER = 116;
 const MAIN_SCOREBOARD_ANIMATION_DURATION_MS = 3000;
